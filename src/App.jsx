2e0c958f3d12e81710fbde6fc38703367b4a3005
Mir Billadin <mir.billadin4@gmail.com>
2023-09-22 23:48:50 +0600

    CSS +basic html added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React, { useState } from 'react';
-import data from './data';
+
+
 function App() {
-  
+  const [count, setCount] = useState(1);
   
   return (
-  <h2>lorem ipsum project setup</h2>
+  <section className='section-center'>
+    <h3>Tired of boring lorem ipsum?</h3>
+    <form action="" className='lorem-form'>
+      <label htmlFor="amount">paragraphs:</label>
+      <input type="number" name="amount" id="amount" min='1' step='1' max='8' value={count} onChange={(e)=> setCount(e.target.value)}/>
+      <button className='btn' typeof='submit'>generate</button>
+    </form>
+  </section>
     )
 }
 
