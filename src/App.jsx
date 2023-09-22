efd414ba29ed815f4feef89eb4686c06a620e695
Mir Billadin <mir.billadin4@gmail.com>
2023-09-23 00:00:15 +0600

    show paragraph functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 import React, { useState } from 'react';
-
+import data from './data';
 
 function App() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
   
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    setText(data.slice(0, amount));
+  }
+
+
   return (
   <section className='section-center'>
     <h3>Tired of boring lorem ipsum?</h3>
-    <form action="" className='lorem-form'>
+    <form className='lorem-form' onSubmit={handleSubmit}>
       <label htmlFor="amount">paragraphs:</label>
       <input type="number" name="amount" id="amount" min='1' step='1' max='8' value={count} onChange={(e)=> setCount(e.target.value)}/>
       <button className='btn' typeof='submit'>generate</button>
     </form>
+    <article>
+      {
+        text.map((item, index) =>{
+          return <p key={index}>{item}</p>
+        })
+      }
+    </article>
   </section>
     )
 }
